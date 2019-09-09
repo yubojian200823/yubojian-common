0652a5c5e0f063195283a912a21ff7d90542a6fb
yubojian <1151022695@qq.com>
2019-09-09 09:28:57 +0800

    周考提交1

--- src/test/java/com/yubojian/common/utils/StringUtilTest.java
+++ src/test/java/com/yubojian/common/utils/StringUtilTest.java
@@ -1,10 +1,5 @@
 package com.yubojian.common.utils;
 
-import static org.junit.Assert.*;
-
-import java.util.Calendar;
-import java.util.Date;
-
 import org.junit.Test;
 
 public class StringUtilTest {
@@ -48,6 +43,37 @@ public class StringUtilTest {
 		String string = StringUtil.generateChineseName();
 		System.out.println(string);
 		 }
+	}
+//测试tohtml方法
+	@Test
+	public void TestTohtml() {
+		String string="9月5日一大早，又有了新情况。\r\n" + 
+				"上午9点多，传出中美经贸高级别磋商超大号传媒\r\n" + 
+				"NSBBKCL。";
+			String html = StringUtil.toHtml(string);
+			System.out.println(html);
+	}
+//	测试手机号工具类是否正确
+	@Test
+	public void TestisPhone() {
+		String phone="12533339999";
+		boolean flag = StringUtil.isPhone(phone);
+		if(flag) {
+			System.out.println("该手机号正确");
+		}else {
+			System.out.println("该手机号有误");
+		}
+	}
+//  测试邮箱号是否正确
+	@Test
+	public void TestisEmail() {
+		String email="A2533339999@qq.com";
+		boolean flag = StringUtil.isEmail(email);
+		if(flag) {
+			System.out.println("该邮箱正确");
+		}else {
+			System.out.println("该邮箱有误");
+		}
 	}
 
 	
